9789c02e0960ae7b58d6353cbc662994656a5083
Trenton Telge <darkforce102@gmail.com>
2020-06-30 16:54:25 -0500

    Added properties display sidebar

--- src/main/java/com/trentontelge/gamemanagerfx/ui/MainController.java
+++ src/main/java/com/trentontelge/gamemanagerfx/ui/MainController.java
@@ -6,15 +6,17 @@ import com.trentontelge.gamemanagerfx.prototypes.Game;
 import com.trentontelge.gamemanagerfx.util.DBFileFilter;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,10 +32,54 @@ public class MainController implements Initializable {
     public TableColumn<Game, Image> ratingCol;
     public TableColumn<Game, String> sizeCol;
     public TableColumn<Game, String> tagsCol;
+    public Label rjCodeDisplay;
+    public Label titleDisplay;
+    public Label circleDisplay;
+    public Label pathDisplay;
+    public Label sizeDisplay;
+    public ImageView ratingDisplay;
+    public Label releaseDateDisplay;
+    public Label tagsDisplay;
+    public Button editGameButton;
     private ObservableList<Game> data;
+    private Game previousSelection;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        gameTable.setRowFactory( tv -> {
+            TableRow<Game> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 1 && (!row.isEmpty())) {
+                    if (!gameTable.getSelectionModel().getSelectedItem().equals(previousSelection)){
+                        previousSelection = gameTable.getSelectionModel().getSelectedItem();
+                        resetLabels();
+                        rjCodeDisplay.setText(previousSelection.getRJCode());
+                        titleDisplay.setText(previousSelection.getTitle());
+                        circleDisplay.setText(previousSelection.getCircleName());
+                        pathDisplay.setText(previousSelection.getPath());
+                        sizeDisplay.setText(String.valueOf(previousSelection.getSize())); //TODO get formatted size
+                        //TODO set rating image
+                        releaseDateDisplay.setText(previousSelection.getReleaseDate().toString());
+                        tagsDisplay.setText(previousSelection.getTags());
+                        //TODO set images
+                    }
+                }
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
+                    if (new File(gameTable.getSelectionModel().getSelectedItem().getPath()).exists()) {
+                        System.out.println("Attempt to run " + gameTable.getSelectionModel().getSelectedItem().getPath());
+                        try {
+                            Runtime.getRuntime().exec(gameTable.getSelectionModel().getSelectedItem().getPath());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        //TODO show error dialog
+                        System.out.println("Game executable not found");
+                    }
+                }
+            });
+            return row ;
+        });
         iconCol.setCellValueFactory(new PropertyValueFactory<>("visibleImage"));
         titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
         circleCol.setCellValueFactory(new PropertyValueFactory<>("circleName"));
@@ -66,5 +112,15 @@ public class MainController implements Initializable {
         data = FXCollections.observableList(DatabaseHelper.getAllGames());
         gameTable.setItems(data);
         System.out.println("Data refreshed.");
+    }
+    protected void resetLabels(){
+        rjCodeDisplay.setText("     ");
+        titleDisplay.setText("     ");
+        circleDisplay.setText("     ");
+        pathDisplay.setText("     ");
+        sizeDisplay.setText("     ");
+        ratingDisplay.setImage(new Image("img\\0.png"));
+        releaseDateDisplay.setText("     ");
+        tagsDisplay.setText("     ");
     }
 }
